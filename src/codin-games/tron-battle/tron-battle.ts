89488c3bb9c3a77bfde5503134a9f1a220db729d
Javier Candalaft <canda@bfaindustries.com>
2021-09-07 21:47:58 -0300

    refactor and fixed bug where we didn't account for users starting position

--- src/codin-games/tron-battle/tron-battle.ts
+++ src/codin-games/tron-battle/tron-battle.ts
@@ -10,68 +10,36 @@ enum Direction {
   Right = 'RIGHT',
 }
 
-let currentDirection: Direction = Direction.Left;
-const playerPaths: { x1: number; y1: number }[][] = [];
+type Position = { x: number; y: number };
 
-const savePlayerPath = (playerIndex: number, x1: number, y1: number) => {
-  playerPaths[playerIndex] = [...(playerPaths[playerIndex] || []), { x1, y1 }];
-};
-const isFreeSquare = (x: number, y: number) => {
-  console.error('isFreeSquare', x, y);
-  if (x > 29) return false;
-  if (y > 19) return false;
-  if (x < 0) return false;
-  if (y < 0) return false;
+let currentDirection: Direction = Direction.Left;
+const playerPaths: Position[][] = [];
+
+const isPositionFree = (position: Position) => {
+  if (position.x > 29) return false;
+  if (position.y > 19) return false;
+  if (position.x < 0) return false;
+  if (position.y < 0) return false;
   if (
-    playerPaths.some((path) => path.some(({ x1, y1 }) => x === x1 && y === y1))
+    playerPaths.some((path) =>
+      path.some(({ x, y }) => x === position.x && y === position.y),
+    )
   )
     return false;
   return true;
 };
 
-const canGoUp = (
-  currentPosition: { x1: number; y1: number },
-  direction: Direction,
-) => {
-  console.error('canGoUp');
-  if (direction === Direction.Down) return false;
-  if (!isFreeSquare(currentPosition.x1, currentPosition.y1 - 1)) return false;
+const canGoUp = (currentPosition: Position) =>
+  isPositionFree({ x: currentPosition.x, y: currentPosition.y - 1 });
 
-  return true;
-};
+const canGoDown = (currentPosition: Position) =>
+  isPositionFree({ x: currentPosition.x, y: currentPosition.y + 1 });
 
-const canGoDown = (
-  currentPosition: { x1: number; y1: number },
-  direction: Direction,
-) => {
-  console.error('canGoDown');
-  if (direction === Direction.Up) return false;
-  if (!isFreeSquare(currentPosition.x1, currentPosition.y1 + 1)) return false;
+const canGoLeft = (currentPosition: Position) =>
+  isPositionFree({ x: currentPosition.x - 1, y: currentPosition.y });
 
-  return true;
-};
-
-const canGoLeft = (
-  currentPosition: { x1: number; y1: number },
-  direction: Direction,
-) => {
-  console.error('canGoLeft');
-  if (direction === Direction.Right) return false;
-  if (!isFreeSquare(currentPosition.x1 - 1, currentPosition.y1)) return false;
-
-  return true;
-};
-
-const canGoRight = (
-  currentPosition: { x1: number; y1: number },
-  direction: Direction,
-) => {
-  console.error('canGoRight');
-  if (direction === Direction.Left) return false;
-  if (!isFreeSquare(currentPosition.x1 + 1, currentPosition.y1)) return false;
-
-  return true;
-};
+const canGoRight = (currentPosition: Position) =>
+  isPositionFree({ x: currentPosition.x + 1, y: currentPosition.y });
 
 // game loop
 while (true) {
@@ -83,10 +51,13 @@ while (true) {
     var inputs: string[] = readline().split(' ');
     const x0: number = parseInt(inputs[0]); // starting X coordinate of lightcycle (or -1)
     const y0: number = parseInt(inputs[1]); // starting Y coordinate of lightcycle (or -1)
-    const x1: number = parseInt(inputs[2]); // starting X coordinate of lightcycle (can be the same as X0 if you play before this player)
-    const y1: number = parseInt(inputs[3]); // starting Y coordinate of lightcycle (can be the same as Y0 if you play before this player)
-    playerPositions.push({ x0, y0, x1, y1 });
-    savePlayerPath(i, x1, y1);
+    const x: number = parseInt(inputs[2]); // starting X coordinate of lightcycle (can be the same as X0 if you play before this player)
+    const y: number = parseInt(inputs[3]); // starting Y coordinate of lightcycle (can be the same as Y0 if you play before this player)
+    playerPositions.push({ x0, y0, x, y });
+    if (!playerPaths[i]) {
+      playerPaths[i] = [{ x: x0, y: y0 }];
+    }
+    playerPaths[i].push({ x, y });
   }
 
   const me = playerPositions[P];
@@ -96,13 +67,13 @@ while (true) {
   console.error({ me });
   // console.error(JSON.stringify({ playerPaths }, null, 2));
 
-  if (canGoRight(me, currentDirection)) {
+  if (canGoRight(me)) {
     currentDirection = Direction.Right;
-  } else if (canGoLeft(me, currentDirection)) {
+  } else if (canGoLeft(me)) {
     currentDirection = Direction.Left;
-  } else if (canGoUp(me, currentDirection)) {
+  } else if (canGoUp(me)) {
     currentDirection = Direction.Up;
-  } else if (canGoDown(me, currentDirection)) {
+  } else if (canGoDown(me)) {
     currentDirection = Direction.Down;
   }
 

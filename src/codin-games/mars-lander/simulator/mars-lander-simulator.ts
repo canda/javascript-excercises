6c402e2d64fac14d06ec904a3a73c7aba4446179
Javier Candalaft <canda@bfaindustries.com>
2021-09-28 08:01:17 -0300

    rocket position mars lander

--- src/codin-games/mars-lander/simulator/mars-lander-simulator.ts
+++ src/codin-games/mars-lander/simulator/mars-lander-simulator.ts
@@ -3,6 +3,10 @@ const marsLanderSimulator = () => {
     x: number;
     y: number;
   };
+  type Velocity = {
+    horizontal: number;
+    vertical: number;
+  };
 
   const FLOOR_POINTS: Coordinate[] = [
     { x: 0, y: 100 },
@@ -13,6 +17,10 @@ const marsLanderSimulator = () => {
     { x: 5500, y: 150 },
     { x: 6999, y: 800 },
   ];
+
+  const INITIAL_POSITION: Coordinate = { x: 2500, y: 2700 };
+  const INITIAL_VELOCITY = { horizontal: 0, vertical: 0 };
+  const INITIAL_ROTATION = 0;
 
   const svgElement = document.querySelector('#simulator-svg');
 
@@ -25,8 +33,24 @@ const marsLanderSimulator = () => {
     FLOOR_POINTS.map((p) => `${p.x},${p.y}`).join(' '),
   );
   floorPolyline.setAttribute('stroke', 'red');
+  floorPolyline.setAttribute('id', 'floor');
   floorPolyline.setAttribute('stroke-width', '20');
   floorPolyline.setAttribute('fill', 'none');
   svgElement.appendChild(floorPolyline);
+
+  let position = INITIAL_POSITION;
+  let velocity = INITIAL_VELOCITY;
+  let rotation = INITIAL_ROTATION;
+
+  const rocketElement = document.querySelector('#rocket');
+  const render = () => {
+    rocketElement.setAttribute(
+      'transform',
+      `translate(${position.x},${position.y}) rotate(${rotation})`,
+    );
+  };
+  render();
+
+  // const loop = (newVelocity: { horizont }) => {};
 };
 marsLanderSimulator();

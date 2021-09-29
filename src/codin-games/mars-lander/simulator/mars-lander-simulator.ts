49e3047b581fb4745d342a8908a1e509cbdda6b8
Javier Candalaft <canda@bfaindustries.com>
2021-09-28 21:52:41 -0300

    simulate run

--- src/codin-games/mars-lander/simulator/mars-lander-simulator.ts
+++ src/codin-games/mars-lander/simulator/mars-lander-simulator.ts
@@ -18,9 +18,11 @@ const marsLanderSimulator = () => {
     { x: 6999, y: 800 },
   ];
 
+  const MARS_GRAVITY = 3.721;
   const INITIAL_POSITION: Coordinate = { x: 2500, y: 2700 };
   const INITIAL_VELOCITY = { horizontal: 0, vertical: 0 };
   const INITIAL_ROTATION = 0;
+  const INITIAL_THRUST = 0;
 
   const svgElement = document.querySelector('#simulator-svg');
 
@@ -38,19 +40,62 @@ const marsLanderSimulator = () => {
   floorPolyline.setAttribute('fill', 'none');
   svgElement.appendChild(floorPolyline);
 
-  let position = INITIAL_POSITION;
-  let velocity = INITIAL_VELOCITY;
-  let rotation = INITIAL_ROTATION;
+  const currentState = {
+    position: INITIAL_POSITION,
+    velocity: INITIAL_VELOCITY,
+    rotation: INITIAL_ROTATION,
+    thrust: INITIAL_THRUST,
+  };
 
   const rocketElement = document.querySelector('#rocket');
   const render = () => {
     rocketElement.setAttribute(
       'transform',
-      `translate(${position.x},${position.y}) rotate(${rotation})`,
+      `translate(${currentState.position.x},${currentState.position.y}) rotate(${currentState.rotation})`,
     );
   };
   render();
 
-  // const loop = (newVelocity: { horizont }) => {};
+  const degreesToRadians = (degrees: number) => (degrees * Math.PI) / 180;
+
+  const gameTurn = (newThrust: number, newRotation: number) => {
+    // calculate accelerations
+    const verticalAcceleration =
+      Math.cos(degreesToRadians(currentState.rotation)) * currentState.thrust -
+      MARS_GRAVITY;
+    const horizontalAcceleration =
+      Math.sin(degreesToRadians(-currentState.rotation)) * currentState.thrust;
+
+    // set new position
+    currentState.position.x =
+      currentState.position.x +
+      currentState.velocity.horizontal +
+      horizontalAcceleration / 2;
+    currentState.position.y =
+      currentState.position.y +
+      currentState.velocity.vertical +
+      verticalAcceleration / 2;
+
+    // set new velocity
+    currentState.velocity.horizontal =
+      currentState.velocity.horizontal + horizontalAcceleration;
+    currentState.velocity.vertical =
+      currentState.velocity.vertical + verticalAcceleration;
+
+    // set input rotation and thrust
+    currentState.rotation = newRotation;
+    currentState.thrust = newThrust;
+
+    console.log(currentState.position);
+    console.log(currentState.velocity);
+    console.log({ verticalAcceleration, horizontalAcceleration });
+
+    // render on svg the new position rotation and thrust
+    render();
+  };
+
+  setInterval(() => {
+    gameTurn(4, 90);
+  }, 1000);
 };
 marsLanderSimulator();

eaffafb6e9b8fa98f6b081b732a6e80fe4efd3f5
Javier Candalaft <jacandalaft@gmail.com>
2025-10-12 21:03:08 +0000

    Oct 12, 2025, 6:03 PM

--- src/leet-code/209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.ts
+++ src/leet-code/209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.ts
@@ -6,8 +6,8 @@ const arraySum = (nums: number[], startingIndex: number, length: number) =>
 function minSubArrayLen(target: number, nums: number[]): number {
   let result = nums.length + 1;
   let foundResult = false;
+  let sum = arraySum(nums, 0, result);
   for (let index = 0; index < nums.length; index++) {
-    let sum = arraySum(nums, index, result);
     for (let arrayLength = result; arrayLength > 0; arrayLength--) {
       if (sum >= target) {
         result = arrayLength;
@@ -21,6 +21,7 @@ function minSubArrayLen(target: number, nums: number[]): number {
         sum -
         (indexToRemove >= nums.length ? 0 : nums[index + arrayLength - 1]);
     }
+    sum = sum - nums[index] + (nums[index + result] || 0);
   }
   if (!foundResult) {
     return 0;
